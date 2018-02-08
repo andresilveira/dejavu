2c580c821c8d0066b717e31151e9270c08f39382
André Herculano <andresilveirah@gmail.com>
2018-02-08 11:43:22 +0100

    use closeconnection client on DripEmailSelect

--- live/src/js/QueryList/DripEmailSelect.js
+++ live/src/js/QueryList/DripEmailSelect.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { getOptionsForDripEmail } from '../helper/closeconnection';
 
 class DripEmailSelect extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      options: []
-    }
+    this.state = { options: [] };
   }
 
   componentWillMount() {
-    this.getOptionsForDripEmail(this.props.field)
-    .then(options => this.setState({ options }));
-  }
-
-  getOptionsForDripEmail(field) {
-    return fetch(`http://localhost:3000/dripemail/choices/?field=${field}`)
-    .then(resp => resp.json());
+    this.props.getOptionsForDripEmail(this.props.field)
+      .then(options => this.setState({ options }));
   }
 
   render() {
-    const { field, ...selectProps } = this.props;
+    const { field, getOptionsForDripEmail, ...selectProps } = this.props;
     return (
       <select {...selectProps} ref={(s) => { this.select = s }}>
         <option value="">Please select...</option>
@@ -32,11 +26,16 @@ class DripEmailSelect extends React.Component {
   }
 }
 
+DripEmailSelect.defaultProps = {
+  getOptionsForDripEmail: getOptionsForDripEmail
+};
+
 DripEmailSelect.propTypes = {
 	field: PropTypes.string.isRequired,
   name: PropTypes.string,
   value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  getOptionsForDripEmail: PropTypes.func.isRequired
 };
 
 export default DripEmailSelect;
